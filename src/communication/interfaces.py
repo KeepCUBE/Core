e6568231e058c4415d7c80ce23d9e60ef50fedc9
root <marek.sefl@seznam.cz>
2017-09-16 12:12:40 +0200

    serial method extraction

--- src/communication/interfaces.py
+++ src/communication/interfaces.py
@@ -1,47 +1,64 @@
 import serial
 from threading import Thread
 
-from config import INTERFACES as config
+#from config import INTERFACES as config
+
+
 
 class Interfaces:
 	"""docstring for Interface"""
 
 	def __init__(self):
 		print('IF: construct')
-		self._baud = config['SERIAL']['baud']
-		self._ser_port = config['SERIAL']['port']
+#		self._baud = config['SERIAL']['baud']
+#		self._ser_port = config['SERIAL']['port']
+		self._baud = 9600
+		self._ser_port = "/dev/ttyS0"
 
-	def set_serial(self, port='/dev/ttyS0', baud=9600):
+
+	def set_serial(self, port, baud):
 		print("IF: serial set")
 		self._ser_port = port
 		self._baud = baud
 
 	def init(self):
 		print('IF: init')
-		self._ser_conn = self.Serial(self._ser_port,self._baud)
+		self._ser_conn = Serial(self._ser_port,self._baud)
+		self.nrf = NRF(self._ser_conn)
 
-	class Serial:
-		ser = None
+class NRF:
+	def __init__(self, serial):
+		self.serial = serial
 
-		def __init__(self, com='/dev/ttyS0', baud=9600):
-			print('serial: construct')
-			global ser
-			ser = serial.Serial(port=com, baudrate=baud, timeout=2)
-			if ser.isOpen(): ser.close()
-			ser.open()
-			#Thread(target = self.listen).start()
+	def send(self, data):
+		print(data)
 
-		def listen(self):
-			print("serial: listen")
-			while True:
-				byte = ser.read(1)
-				if byte:
-					print(byte.decode())
 
-		@staticmethod
-		def send(data):
-			print(ser.write(data.encode()))
+class Serial:
+	ser = None
+	def __init__(self, com='/dev/ttyS4', baud=9600):
+		print('serial: construct')
+		global ser
+		ser = serial.Serial(port=com, baudrate=baud, timeout=2)
+		if ser.isOpen(): ser.close()
+		ser.open()
+		#Thread(target = self.listen).start()
 
-		def __del__(self):
-			print('serial: destruct')
-			ser.close()
+	def listen(self):
+		print("serial: listen")
+		while True:
+			byte = ser.read(1)
+			if byte:
+				print(byte.decode())
+
+	@staticmethod
+	def send(data):
+		print(ser.write(data.encode()))
+
+	def __del__(self):
+		print('serial: destruct')
+		ser.close()
+
+ifaces = Interfaces()
+ifaces.init()
+ifaces.nrfs.send("ahoj")
