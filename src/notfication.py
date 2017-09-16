be1e66226cb515d54b240d3339c3021878e720ef
root <marek.sefl@seznam.cz>
2017-09-16 16:23:33 +0200

    Added notfication class

--- src/notfication.py
+++ src/notfication.py
@@ -1,10 +1,14 @@
 import json
 
-msgType = "notification"
-ntfType = "warn"
-title = "nadpis"
-content = "obsah"
-color = None
 
-
-print(json.dumps([{'type': msgType, 'data': {'type': ntfType, 'title': title, 'content': content, 'color': color}}]))
+class Notification:
+	@staticmethod
+	def send(id, notify_type, title, content, color="#ff0000"):
+		if (id >= 2147483647) and (id < 0):
+			raise ValueError("Notification ID exceeds 4 bytes")
+		else:
+			print(json.dumps(
+				[{
+					'type': 'notification',
+					'data': {'id': id, 'type': notify_type, 'title': title, 'content': content, 'color': color}
+				}]))
